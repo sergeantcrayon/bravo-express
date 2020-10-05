2bc7cffef2507ef90ebb824c25a78cd145697270
Victor Bravo <vicbravofreelancer@gmail.com>
2020-10-05 17:51:39 -0400

    join route

--- src/routes/lfg.ts
+++ src/routes/lfg.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { authorizeToken } from '../middleware/auth';
 import Lfg from '../models/lfg.model';
-import { IUser } from '../models/user.model';
+import { IUser, UserSchema } from '../models/user.model';
 const router = express.Router();
 
 router.post('/', [authorizeToken], (req, res) => {
@@ -39,4 +39,18 @@ router.post('/query', async (req, res) => {
   res.send(lfgs);
 });
 
+router.post('/join', [authorizeToken], async (req, res) => {
+  const params: { ign: string; lfgId: string } = req.body;
+  const user: IUser = req['user']['_doc'];
+  const lfg = await Lfg.findOne({ _id: params.lfgId });
+  if (lfg.users.length >= lfg.maxPlayers) {
+    res.status(403).send({ message: 'Forbidden: Lfg room is full' });
+  } else if (lfg.users.findIndex((u) => u.googleId === user.googleId) >= 0) {
+    return res.status(403).send({ message: 'You already joined this Lfg.' });
+  } else {
+    lfg.users.push({ ...user, ign: params.ign });
+    lfg.save();
+    res.send(lfg);
+  }
+});
 export default router;
